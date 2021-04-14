f6cb2ddc06b00718fcb59d2a55586cecc2bb66fc
Myhat2you <joshuadtalbot3@gmail.com>
2021-04-14 19:32:18 +0100

    update

--- web/demo.php
+++ web/demo.php
@@ -1,17 +1,39 @@
 <?php
-    $uapp = "ls -al";
-    $cmd = $uapp . $_POST["pinno"] . " " . $_POST["pinval"]. '';
+    $max_machines = 5;
 
-    echo "User Test App" . $cmd;
+    $name = $loc = $pin = $status = "";
 
-    $output = shell_exec($cmd);
-    echo "<pre>$output</pre>";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = test_input($_POST["name"]);
+        $loc = test_input($_POST["location"]);
+        $pin = test_input($_POST["pin"]);
+        $stat = test_input($_POST["status"]);
+      }
+      
+      function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
 ?>
 
-<form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
+<form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?> "
     <input type="submit" class="button" name="btnAddMachine" value="Add Machine" />
     Machine Number:
-    <input type="number" name="numMachine"  id="mnum" min="1" max="5"/> <br>
-    <input type="submit" name="btnSendForm" value="Delete Machine" />
-    <input type="submit" name="btnSendForm" value="Update Machine" />
+    <input type="number" name="numMachine"  id="mNum" min="1" max="5"/> <br>
+    <input type="submit" name="btnDelete" value="Delete Machine" />
+    <input type="submit" name="btnUpdate" value="Update Machine" />
+</form>
+
+<?php
+?>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?> "
+    Name:       <input type="text"   name="txtName"     id="mName"/> <br>
+    Location:   <input type="text"   name="txtLocation" id="mLoc" /> <br>
+    Pin:        <input type="number" name="numPin"      id="mPin"   min="0" max="27"/> <br>
+    Status:     <input type="status" name="numStatus"   id="mStat"  min="0" max="1"/> <br>
+
+    <input type="submit" name="btnSendUpdate" value="Add Machine" />
 </form>
