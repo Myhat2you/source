239b2aab9ce808215309cf14e21bdd944c98a87d
Myhat2you <joshuadtalbot3@gmail.com>
2021-04-15 04:47:15 +0100

    update

--- web/demo.php
+++ web/demo.php
@@ -4,11 +4,10 @@
     $name = $loc = $pin = $status = "";
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $name = $_POST["txtName"];
-        //$loc = test_input($_POST["txtLocation"]);
-        //$pin = test_input($_POST["numPin"]);
-        //$stat = test_input($_POST["numStatus"]);
-        print "Hello, $name\n";
+        $name = test_input($_POST["name"]);
+        $loc = test_input($_POST["location"]);
+        $pin = test_input($_POST["pin"]);
+        $stat = test_input($_POST["status"]);
       }
       
       function test_input($data) {
@@ -18,7 +17,19 @@
         return $data;
       }
 ?>
-<form method="post" action="/cgi-bin/output.o">
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?> "
+    <input type="submit" class="button" name="btnAddMachine" value="Add Machine" />
+    Machine Number:
+    <input type="number" name="numMachine"  id="mNum" min="1" max="5"/> <br>
+    <input type="submit" name="btnDelete" value="Delete Machine" />
+    <input type="submit" name="btnUpdate" value="Update Machine" />
+</form>
+
+<?php
+?>
+
+<form method="post" action="/cgi-bin/output test">
     Name:       <input type="text"   name="txtName"     id="mName"/> <br>
     Location:   <input type="text"   name="txtLocation" id="mLoc" /> <br>
     Pin:        <input type="number" name="numPin"      id="mPin"   min="0" max="27"/> <br>
