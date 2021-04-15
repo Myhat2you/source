1044877c453324a810ac11c9c325daf5d0c35f81
Myhat2you <joshuadtalbot3@gmail.com>
2021-04-15 07:20:33 +0100

    update

--- web/demo.php
+++ web/demo.php
@@ -8,7 +8,7 @@
         //$loc = test_input($_POST["txtLocation"]);
         //$pin = test_input($_POST["numPin"]);
         //$stat = test_input($_POST["numStatus"]);
-        print "Hello there\n";
+        print "Hello there, $name\n";
     }
 
       
