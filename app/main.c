8e54d9c34c9ba84e077f5e78caaa55dfaab1c403
Myhat2you <joshuadtalbot3@gmail.com>
2021-04-15 06:02:38 +0100

    update

--- app/main.c
+++ app/main.c
@@ -268,7 +268,7 @@ int main(int argc, char** argv) {
 		}*/
 		web_print_header();
 
-		printf("<h1>Hello World!</h1>");
+		//printf("<h1>Hello World!</h1>");
 		printf("<h2>arg: %s</h2>", argv[2]);
 		return 1;
 	}
