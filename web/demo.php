eada002e0c2a9a540d7f73e8702a9dafba82f767
Myhat2you <joshuadtalbot3@gmail.com>
2021-04-15 08:42:52 +0100

    update

--- web/demo.php
+++ web/demo.php
@@ -1,6 +1,5 @@
 <?php
     $max_machines = 5;
-
     $name = $loc = $pin = $status = "";
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -19,11 +18,3 @@
         return $data;
       }
 ?>
-<form method="post" action="/cgi-bin/output.o">
-    Name:       <input type="text"   name="txtName"     id="mName"/> <br>
-    Location:   <input type="text"   name="txtLocation" id="mLoc" /> <br>
-    Pin:        <input type="number" name="numPin"      id="mPin"   min="0" max="27"/> <br>
-    Status:     <input type="status" name="numStatus"   id="mStat"  min="0" max="1"/> <br>
-
-    <input type="submit" name="btnSendUpdate" value="Add Machine" />
-</form>
